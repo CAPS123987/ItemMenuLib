fff414e809cf253487173be69c06ca370e19fea0
CAPS123987 <87692752+CAPS123987@users.noreply.github.com>
2024-02-04 18:46:35 +0100

    Delete test2.bat

--- test2.bat
+++
@@ -1,3 +0,0 @@
-xcopy "C:\Users\hnili\Documents\GitHub\ItemMenuLib\target\ItemMenuLib-1.0.jar" "C:\Users\hnili\Desktop\kill get money\DungeonGenerator\server\plugins\ItemMenuLib-1.0.jar" /U /Y
-cd "C:\Users\hnili\Desktop\kill get money\DungeonGenerator\server"
-start.bat
